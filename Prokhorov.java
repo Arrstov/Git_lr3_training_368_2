a3e603e6555011837010aabbdd56c5179df22310
Maksimka <Max00-8@yandex.ru>
2026-03-18 20:50:42 +0300

    второй пуки как в свою ветку

--- Prokhorov.java
+++ Prokhorov.java
@@ -15,7 +15,7 @@ public class App {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // второй пуки как в свою ветку
         App app = new App();
         app.add("First message");
         app.add("Second message");
